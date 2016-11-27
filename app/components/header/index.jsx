1aeb76cd61575e3566ecca8cee3c907c41e943fa
Pranesh Ravi <praneshpranesh@gmail.com>
2016-11-27 18:22:52 +0530

    Updating Header component

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -1,15 +1,36 @@
 import React, { Component, PropTypes } from 'react'
-
-import { allowMultiple } from '../../utils'
 import reactStyles from 'react-css-modules'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { APIs } from '../../APIs/'
+import { actions } from '../../actions'
 import style from './style'
 
-@reactStyles(style, allowMultiple)
+
+const uiStates = states => ({
+  ui: states.reducer,
+})
+
+const uiActions = dispatch => ({
+  actions: bindActionCreators({
+    ...actions,
+    ...APIs,
+  }, dispatch),
+})
+
+
+@connect(uiStates, uiActions)
+@reactStyles(style)
 export default class Sample extends Component {
   render() {
     return (
       <div styleName="header">
-        <h1>React</h1>
+        <a
+          styleName="link"
+          href="https://github.com/praneshr/smart-react-boilerplate"
+        >
+          Fork me on Github
+        </a>
       </div>
     )
   }
