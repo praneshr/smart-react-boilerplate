77899e8a6b0ac34e3a294efae50ec2c7cf237441
Pranesh Ravi <praneshpranesh@gmail.com>
2016-11-27 18:24:29 +0530

    Misc fixes, removing unwanted code and files,

--- app/components/not_found/index.jsx
+++ app/components/not_found/index.jsx
@@ -1,9 +1,6 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 export default class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
--- app/router.jsx
+++ app/router.jsx
@@ -1,6 +1,5 @@
 import { Redirect, Route, Router, browserHistory } from 'react-router'
 import React from 'react'
-import About from './pages/about'
 import Intro from './pages/intro'
 import Layout from './layout'
 import NotFound from './components/not_found/'
@@ -11,7 +10,6 @@ export default () => (
     <Redirect from="/base" to="/base/intro" />
     <Route path="/base" component={Layout}>
       <Route path="intro" component={Intro} />
-      <Route path="about" component={About} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
