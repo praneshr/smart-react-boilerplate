e6695af60925d2bee58e3a0f20080a91035b6730
Pranesh Ravi <praneshpranesh@gmail.com>
2016-11-27 18:28:39 +0530

    Creating an awesome intro page :fire:

--- app/pages/intro/index.jsx
+++ app/pages/intro/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import reactStyles from 'react-css-modules'
+import style from './style'
+import banner from '../../globals/assets/future.png'
 
+@reactStyles(style)
 export default class Intro extends Component {
 
   render() {
     return (
-      <div className="Intro">
-        <button className="btn btn-primary">reds</button>
+      <div styleName="intro">
+        <h1 styleName="heading">Smart React Boilerplate</h1>
+        <img src={banner} alt="" />
       </div>
     )
   }
